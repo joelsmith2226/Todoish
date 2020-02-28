48ab3c0e02c9ba2834cdc3677361a303116ab309
joelsmith2226 <joel.smith2226@gmail.com>
2020-02-28 14:41:47 +1100

    Updates

---
+++ precache-manifest.97b3f06d89b37538c358a5f78693f1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeb0e13d604f9ba4da0c2b9c2500e1e",
+    "url": "/Todoish/index.html"
+  },
+  {
+    "revision": "b2da0b6a3289fbefc769",
+    "url": "/Todoish/static/css/main.301cf51d.chunk.css"
+  },
+  {
+    "revision": "494ec29a638f6ada4c81",
+    "url": "/Todoish/static/js/2.366d56c0.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/Todoish/static/js/2.366d56c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2da0b6a3289fbefc769",
+    "url": "/Todoish/static/js/main.a99dae24.chunk.js"
+  },
+  {
+    "revision": "02591f3ccf89b2639794",
+    "url": "/Todoish/static/js/runtime-main.ca778d0b.js"
+  }
+]);
